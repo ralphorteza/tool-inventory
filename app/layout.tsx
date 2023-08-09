76213884c4ca8ded2d91602eb3c1bb33c6a022b1
ralph orteza <ralphlorteza@gmail.com>
2023-08-09 16:22:28 -0700

    reset and style app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,23 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className='bg-gray-500'>
+      <body className="bg-gray-100 overflow-y-scroll bg-[url('/grid.svg)] pb-36">
+        {/* <GlobalNav /> */}
+        <div className='lg:pl-72'>
+          <div className='mx-auto max-w-4xl space-y-8 px-2 pt-20 lg:px-8 lg:py-8'>
+            <div className='bg-vc-border-gradient rounded-lg p-px shadow-lg shadow-black/20'>
+              <div className='rounded-lg bg-black'>
+                {/* <AddressBar /> */}
+              </div>
+            </div>
+
+            <div className='bg-vc-border-gradient rounded-lg p-px shadow-lg shadow-black/20'>
+              <div className='rounded-lg bg-black p-3.5 lg:p-6'>{children}</div>
+            </div>
+          </div>
+        </div>
+      </body>
     </html>
   )
 }
