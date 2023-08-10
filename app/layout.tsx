719ba31f11b0b9fdad5f91bd43588e4e2930f8b7
ralph orteza <ralphlorteza@gmail.com>
2023-08-10 14:41:50 -0700

    Work on layout skeleton

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-
+import { GlobalNav } from '@/ui/global-nav'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -15,20 +15,23 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className='bg-gray-500'>
-      <body className="overflow-y-scroll bg-[url('/grid.svg')] pb-36">
-        {/* <GlobalNav /> */}
+    <html lang='en' className='bg-gray-400'>
+      <body className='overflow-y-scroll bg-white pb-36'>
+        <GlobalNav />
         <div className='lg:pl-72'>
-          <div className='mx-auto max-w-4xl space-y-8 px-2 pt-20 lg:px-8 lg:py-8'>
+          <div className='mx-auto max-w-4xl space-y-8 bg-blue-500 px-2 pt-20 lg:px-8 lg:py-8'>
+            
             <div className='bg-vc-border-gradient rounded-lg p-px shadow-lg shadow-black/20'>
               <div className='rounded-lg bg-black'>
                 {/* <AddressBar /> */}
+                <div className='p-2 bg-white rounded-lg'></div> {/* temp dummy for address bar */}
               </div>
             </div>
 
             <div className='bg-vc-border-gradient rounded-lg p-px shadow-lg shadow-black/20'>
               <div className='rounded-lg bg-black p-3.5 lg:p-6'>{children}</div>
             </div>
+
           </div>
         </div>
       </body>
